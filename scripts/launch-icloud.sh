6c8c88a2369f3aef8a611e584693434b8ad26063
santisbon <>
2025-11-07 13:20:05 -0600

    remove unnecessary quotes

--- scripts/launch-icloud.sh
+++ scripts/launch-icloud.sh
@@ -6,37 +6,37 @@ SERVICE="$1"
 
 case "$SERVICE" in
     mail)
-        URL="https://www.icloud.com/mail"
+        URL=https://www.icloud.com/mail
         ;;
     contacts)
-        URL="https://www.icloud.com/contacts"
+        URL=https://www.icloud.com/contacts
         ;;
     calendar)
-        URL="https://www.icloud.com/calendar"
+        URL=https://www.icloud.com/calendar
         ;;
     photos)
-        URL="https://www.icloud.com/photos"
+        URL=https://www.icloud.com/photos
         ;;
     drive)
-        URL="https://www.icloud.com/iclouddrive"
+        URL=https://www.icloud.com/iclouddrive
         ;;
     notes)
-        URL="https://www.icloud.com/notes"
+        URL=https://www.icloud.com/notes
         ;;
     reminders)
-        URL="https://www.icloud.com/reminders"
+        URL=https://www.icloud.com/reminders
         ;;
     pages)
-        URL="https://www.icloud.com/pages"
+        URL=https://www.icloud.com/pages
         ;;
     numbers)
-        URL="https://www.icloud.com/numbers"
+        URL=https://www.icloud.com/numbers
         ;;
     keynote)
-        URL="https://www.icloud.com/keynote"
+        URL=https://www.icloud.com/keynote
         ;;
     find)
-        URL="https://www.icloud.com/find"
+        URL=https://www.icloud.com/find
         ;;
     *)
         echo "Usage: $0 {mail|contacts|calendar|photos|drive|notes|reminders|pages|numbers|keynote|find}"
@@ -48,4 +48,4 @@ esac
 # All services share ONE profile so you only log in once
 # Epiphany must be installed separately: flatpak install flathub org.gnome.Epiphany
 # Use -- to separate options from arguments to prevent URL being treated as search
-exec flatpak-spawn --host flatpak run org.gnome.Epiphany -- "$URL"
+exec flatpak-spawn --host flatpak run org.gnome.Epiphany -- $URL
